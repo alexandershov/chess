e41487a4c7c0b0d5403d4dc28ee7ce5cb9158287
aershov <codumentary.com@gmail.com>
2020-04-03 19:57:09 +0300

    enable BlockArguments

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,17 @@
+{-# LANGUAGE BlockArguments #-}
+
 import Test.Hspec
 
 import Uci.Input
 
 main :: IO ()
-main = hspec $ do
-    describe "Uci.Input.parse" $ do
-        it "parses `uci` command" $ do
+main = hspec do
+    describe "Uci.Input.parse" do
+        it "parses `uci` command" do
             Uci.Input.parse "uci" `shouldBe` Uci.Input.Uci
 
-        it "parses `isready` command" $ do
+        it "parses `isready` command" do
             Uci.Input.parse "isready" `shouldBe` Uci.Input.IsReady
 
-        it "returns Unknown on unknown commands" $ do
+        it "returns Unknown on unknown commands" do
             Uci.Input.parse "parse this" `shouldBe` Uci.Input.Unknown "parse this"
-
