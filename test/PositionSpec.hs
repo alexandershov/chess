4fffdd2fb04dc6aba78ce881786b550ed3757623
aershov <codumentary.com@gmail.com>
2020-04-09 21:47:59 +0300

    uncomment tests

--- test/PositionSpec.hs
+++ test/PositionSpec.hs
@@ -12,23 +12,19 @@ import Squares
 describeRookMoves :: Spec
 describeRookMoves = do
     describe "rookMoves" do
-        let board = put [whiteRook `on` a1, 
-                         whiteKing `on` e1, 
-                         blackKnight `on` a5,
-                         blackKing `on` e8] 
-            position = Position board White in
             it "moves horizontally" do
-                allMoves position `shouldContain` [Move a1 d1]
-        --     it "moves vertically" do
-        --         allMoves position `shouldContain` [Move a1 a4]
-        --     it "takes enemy piece" do
-        --         allMoves position `shouldContain` [Move a1 a5]
-        --     it "doesn't take own piece" do
-        --         allMoves position `shouldNotContain` [Move a1 e1]
-        --     it "can't move past enemy piece" do
-        --         allMoves position `shouldNotContain` [Move a1 a6]
-        --     it "can't move past own piece" do
-        --         allMoves position `shouldNotContain` [Move a1 f1]
+                allMoves rookTestPosition `shouldContain` [Move a1 d1]
+            it "moves vertically" do
+                allMoves rookTestPosition `shouldContain` [Move a1 a4]
+            it "takes enemy piece" do
+                allMoves rookTestPosition `shouldContain` [Move a1 a5]
+            it "doesn't take own piece" do
+                allMoves rookTestPosition `shouldNotContain` [Move a1 e1]
+            it "can't move past enemy piece" do
+                allMoves rookTestPosition `shouldNotContain` [Move a1 a6]
+            it "can't move past own piece" do
+                allMoves rookTestPosition `shouldNotContain` [Move a1 f1]
+
 
 on :: Piece -> Square -> (Piece, Square)
 piece `on` square = (piece, square)
@@ -36,6 +32,15 @@ piece `on` square = (piece, square)
 put :: [(Piece, Square)] -> Board
 put piecesOnSquares = 
     emptyBoard // [(square, Just piece) | (piece, square) <- piecesOnSquares]
+
+
+rookTestPosition :: Position
+rookTestPosition =
+    let board = put [whiteRook `on` a1, 
+                     whiteKing `on` e1, 
+                     blackKnight `on` a5,
+                     blackKing `on` e8] in
+            Position board White
 
 whiteRook :: Piece
 whiteRook = Rook White
