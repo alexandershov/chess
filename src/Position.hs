93749fa9e19479ba2c789c2f83d2ea2223a2d795
aershov <codumentary.com@gmail.com>
2020-04-09 21:41:45 +0300

    implement rook moves

--- src/Position.hs
+++ src/Position.hs
@@ -1,12 +1,12 @@
 module Position where
 
-import Data.Array (Array, (!), listArray)
-import Data.Maybe (isJust)
+import Data.Array (assocs, Array, (!), listArray)
+import Data.Maybe (isNothing)
 
 boardSize :: Int
 boardSize = 8
 
-data Color = Black | White
+data Color = Black | White deriving (Eq, Show)
 data Piece = 
     Pawn Color | 
     Knight Color | 
@@ -25,14 +25,25 @@ type Board = Array Square (Maybe Piece)
 data Position = Position { getBoard :: Board, getSideToMove :: Color }
 data Move = Move { from :: Square, to :: Square } deriving (Eq)
 
+type Direction = (Int, Int)
+type Range = Int
+
+data Movement = Movement [Direction] Range
+
 instance Show Move where
     show move = showSquare (from move) ++ showSquare (to move)
 
 emptyBoard :: Board
 emptyBoard = listArray ((1, 1), (8, 8)) $ replicate (boardSize * boardSize) Nothing
 
-isOccupied :: Square -> Board -> Bool
-square `isOccupied` board = isJust $ board ! square
+isOnBoard :: Square -> Bool
+isOnBoard (x, y) = and $ map coordinateIsOnBoard [x, y]
+
+coordinateIsOnBoard :: Int -> Bool
+coordinateIsOnBoard x = (x >= 1) && (x <= boardSize)
+
+isEmpty :: Board -> Square -> Bool
+isEmpty board square = isNothing $ board ! square
 
 allMoves :: Position -> [Move]
 allMoves position = 
@@ -42,18 +53,50 @@ allMoves position =
     ]
 
 
-piecesInSquares :: Position -> [(Piece, Squares)]
-piecesInSquares Position board _ =
-    [ (piece, square) | (square, Just piece) <- assocs board ]
+piecesInSquares :: Position -> [(Piece, Square)]
+piecesInSquares (Position board sideToMove) =
+    [ (piece, square) | 
+      (square, Just piece) <- assocs board, 
+      getColor piece == sideToMove ]
 
 
 pieceMoves :: Position -> Piece -> Square -> [Move]
-pieceMoves (Position board _) piece square = []
+pieceMoves (Position board sideToMove) piece square = 
+    let destinations = getDestinations square (getMovement piece)
+        candidates = takeWhileAndNext (isEmpty board) destinations
+        tos = takeWhile (notColoredAs board sideToMove) candidates in
+           map (Move square) tos
 
-type Direction = (Int, Int)
-type Range = Int
 
-data Movement = Movement [Direction] Range
+notColoredAs :: Board -> Color -> Square -> Bool
+notColoredAs board color square =
+    case maybePiece of
+        Nothing -> True
+        Just piece -> (getColor piece) /= color
+    where maybePiece = board ! square
+
+takeWhileAndNext :: (a -> Bool) -> [a] -> [a]
+takeWhileAndNext p xs = 
+    good ++ take 1 bad
+    where (good, bad) = span p xs
+
+
+getDestinations :: Square -> Movement -> [Square]
+getDestinations square (Movement directions range) =
+    filter isOnBoard [ squareInDirection square d i | d <- directions, i <- [1..range] ]
+
+
+mulDirection :: Direction -> Int -> Direction
+mulDirection (x, y) n = (x * n, y * n)
+
+
+addDelta :: Square -> Direction -> Square
+addDelta (x, y) (dx, dy) = (x + dx, y + dy)
+
+squareInDirection :: Square -> Direction -> Range -> Square
+squareInDirection square direction range = 
+    addDelta square $ mulDirection direction range
+
 
 getMovement :: Piece -> Movement
 getMovement (Pawn _) = error "TODO: implement pawn movement"
@@ -62,6 +105,14 @@ getMovement (Bishop _) = Movement diagonals boardSize
 getMovement (Rook _) = Movement straightLines boardSize
 getMovement (Queen _) = Movement (diagonals ++ straightLines) boardSize
 getMovement (King _) = Movement (diagonals ++ straightLines) 1
+
+getColor :: Piece -> Color
+getColor (Pawn color) = color
+getColor (Knight color) = color
+getColor (Bishop color) = color
+getColor (Rook color) = color
+getColor (Queen color) = color
+getColor (King color) = color
 
 jumps :: [Direction]
 jumps = [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
