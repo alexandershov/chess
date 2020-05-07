68b9c0c30d7b4ce8b36a0b1e056163e3b06f261b
aershov <codumentary.com@gmail.com>
2020-05-07 19:16:46 +0300

    parse fen half move clock

--- src/Fen.hs
+++ src/Fen.hs
@@ -1,5 +1,7 @@
 module Fen where
 
+import Text.Read (readEither)
+
 import Pieces
 
 type ErrorDesc = String
@@ -8,3 +10,7 @@ parseSideToMove :: String -> Either ErrorDesc Color
 parseSideToMove "w" = Right White
 parseSideToMove "b" = Right Black
 parseSideToMove s = Left $ "side to move should either `w` or `b`, got " ++ s
+
+
+parseHalfMoveClock :: String -> Either ErrorDesc Int
+parseHalfMoveClock s = readEither s
--- test/FenSpec.hs
+++ test/FenSpec.hs
@@ -15,5 +15,11 @@ describeFen = do
         it "parses side to move" do
             parseSideToMove "w" `shouldBe` Right White
             parseSideToMove "b" `shouldBe` Right Black
-        it "handles errors when parses side to move" do
+        it "handles errors when parsing side to move" do
             parseSideToMove "x" `shouldSatisfy` isLeft
+
+        it "parses half move clock" do
+            parseHalfMoveClock "10" `shouldBe` Right 10
+
+        it "handles errors when parsing half move clock" do
+            parseHalfMoveClock "x" `shouldSatisfy` isLeft
