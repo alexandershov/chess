33aa2f85c7d41aa21fedb0e51589d1a3a5e74e63
aershov <codumentary.com@gmail.com>
2020-08-31 20:25:42 +0300

    move max logic to one place

--- src/Engine.hs
+++ src/Engine.hs
@@ -13,9 +13,6 @@ import Position as P
 maxDepth :: Int
 maxDepth = 2
 
-type Pickers = [[Int] -> Int]
-
-
 findBestMove :: Position -> Move
 findBestMove position@Position{P.sideToMove} = 
     findBestMoveAtDepth sideToMove 0 position
@@ -23,29 +20,37 @@ findBestMove position@Position{P.sideToMove} =
 
 findBestMoveAtDepth :: Color -> Int -> Position -> Move
 findBestMoveAtDepth maximizingPlayer depth position =
-    getMove $ head $ sortOn bestScoreFirst scoredMoves
-    where scoredMoves = getNextScoredMoves maximizingPlayer depth pickers position
-          pickers = cycle [minimum, maximum]
+    getMove $ head scoredMoves
+    where scoredMoves = getNextScoredMoves maximizingPlayer depth position
 
 
-miniMaxEval :: Color -> Int -> Pickers -> Position -> Int
-miniMaxEval maximizingPlayer depth pickers position
+miniMaxEval :: Color -> Int -> Position -> Int
+miniMaxEval maximizingPlayer depth position
     | depth == maxDepth = colorize maximizingPlayer (eval position)
     | moves == [] = colorize maximizingPlayer (eval position)
-    | otherwise = picker $ map getScore $ getNextScoredMoves maximizingPlayer depth nextPickers position
+    | otherwise = head $ map getScore $ getNextScoredMoves maximizingPlayer depth position
     where moves = legalMoves position
-          picker:nextPickers = pickers
 
 
-getNextScoredMoves :: Color -> Int -> Pickers -> Position -> [(Int, Move)]
-getNextScoredMoves maximizingPlayer depth pickers position =
-    [ ((miniMaxEval maximizingPlayer (depth + 1) pickers p), m) | (p, m) <- nextPositionsWithMoves ]
+getNextScoredMoves :: Color -> Int -> Position -> [(Int, Move)]
+getNextScoredMoves maximizingPlayer depth position =
+    applyMiniMax depth scoredMoves
     where nextPositionsWithMoves = [ ((makeUnchecked position move), move) | move <- moves ]
           moves = legalMoves position
+          scoredMoves = [ ((miniMaxEval maximizingPlayer (depth + 1) p), m) | (p, m) <- nextPositionsWithMoves ]
 
 
-bestScoreFirst :: (Int, Move) -> Int
-bestScoreFirst (score, _) = -score
+applyMiniMax :: Int -> [(Int, Move)] -> [(Int, Move)]
+applyMiniMax depth scoredMoves
+    | even depth = sortOn scoreForMaximizing scoredMoves
+    | otherwise = sortOn scoreForMinimizing scoredMoves
+
+
+scoreForMaximizing :: (Int, Move) -> Int
+scoreForMaximizing (score, _) = -score
+
+scoreForMinimizing :: (Int, Move) -> Int
+scoreForMinimizing (score, _) = score
 
 getMove :: (Int, Move) -> Move
 getMove = snd
